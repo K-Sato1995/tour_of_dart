8ed8fc6f314c5315d933142cf413853ab27f555f
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-08-21 16:40:58 +0900

    Add basics

---
+++ basics/main.dart
@@ -0,0 +1,16 @@
+void printInt (int num) {
+  print(num);
+}
+
+// Variable
+var name = 'K-Sato';
+
+// Uninitialized variables that have a nullable type have an initial value of null. (
+int? numb;
+
+
+void main() {
+  printInt(2);
+  print(name);
+}
+
